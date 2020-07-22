aace071554d5abb97c87539d746f8cf25ce75e43
Marcus Davanço <md.daves@gmail.com>
2020-07-22 16:26:10 -0300

    feat: suport for asynchronous operations with thunk.

--- app/actions/status.js
+++ app/actions/status.js
@@ -1,6 +1,14 @@
-import { actionTypes } from '../constants/actionTypes';
+import { actionTypes } from '../constants/actionTypes.js';
 
-export const changeStatus = (userName) => ({
-  type: actionTypes.CHANGE_STATUS,
-  payload: `${userName} is typing`,
-});
+export const changeStatus = (text) => {
+  return (dispatch) => {
+    setTimeout(
+      () =>
+        dispatch({
+          type: actionTypes.CHANGE_STATUS,
+          payload: `${text} is typing...`,
+        }),
+      1000
+    );
+  };
+};
